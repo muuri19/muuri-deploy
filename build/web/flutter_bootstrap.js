b6af645362550e2eafc0d4837df47120646e639c
muuri19 <muhammadsadry19@gmail.com>
2025-02-16 10:49:14 +0700

    add feat: icon launcher

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2052604097"
+    serviceWorkerVersion: "1825965135"
   }
 });
